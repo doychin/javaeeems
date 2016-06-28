09f993289b552642e2b8e042f38f4a3ab9896d5f
Doychin Bondzhev <doychin@dsoft-bg.com>
2016-06-28 19:35:43 +0300

    Event initial commit

--- src/main/java/bg/jug/domain/Event.java
+++ src/main/java/bg/jug/domain/Event.java
@@ -1,7 +1,52 @@
 package bg.jug.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * Created by doychin on 28.6.2016 г..
  */
-public class Event {
+@Entity
+public class Event implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private Date eventTime;
+
+    @Column
+    private String name;
+
+    public Event() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(Date eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
