f0b9e83d53d2884c77cdf5a4434635aff28c9d0e
Doychin Bondzhev <doychin@dsoft-bg.com>
2016-06-28 20:10:02 +0300

    Add relations to event

--- src/main/java/bg/jug/domain/Event.java
+++ src/main/java/bg/jug/domain/Event.java
@@ -5,20 +5,34 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Created by doychin on 28.6.2016 г..
  */
 @Entity
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Event implements Serializable {
 
     @Id
     @GeneratedValue
+    @XmlElement
     private long id;
 
     @Column
+    @XmlElement(required=true)
     private Date eventTime;
 
     @Column
+    @XmlElement(required=true)
     private String name;
 
     @ManyToMany
